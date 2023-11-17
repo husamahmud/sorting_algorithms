e8346f21f8a2077e56005aa7f0374367b2161fa7
iamhusam <husamahmud@gmail.com>
2023-11-17 21:26:49 +0300

    Update header file

--- sort.h
+++ sort.h
@@ -24,5 +24,9 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+void swap(int *x, int *y);
+void swap_nodes(listint_t **list, listint_t *curr, listint_t *prev_node);
 
 #endif

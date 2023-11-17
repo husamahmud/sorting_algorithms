971df2e5ff2164fb7bff03d13b247bbbda9772ea
iamhusam <husamahmud@gmail.com>
2023-11-17 19:19:11 +0300

    Edit 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,42 @@
 #include "sort.h"
 
+void swap(int *x, int *y);
+
+/**
+ * bubble_sort - Bubble Sort Algorithm.
+ *
+ * @array: Pointer to the array to be sorted.
+ * @size: The size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swapped;
+
+	/* check for invalid input or arrays with 1 or fewer elements */
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+
+		/* compare neighboring items and switch if necessary */
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		/* if no elements were swapped, the array is already sorted */
+		if (swapped == 0)
+			break;
+	}
+}
+
 /**
  * swap - Swaps two values of two integers.
  *
@@ -11,36 +48,4 @@ void swap(int *x, int *y)
 	int temp = *x;
 	*x = *y;
 	*y = temp;
-}
-
-/**
- * bubble_sort - sorts an array of integers in ascending order using the Bubble
- * sort algorithm.
- *
- * @array: Pointer to the array to be sorted.
- * @size: The size of the array.
- */
-void bubble_sort(int *array, size_t size)
-{
-	size_t i, j;
-	int swapped;
-
-	if (array == NULL || size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		swapped = 0;
-		for (j = 0; j < size - i - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				swap(&array[j], &array[j + 1]);
-				swapped = 1;
-				print_array(array, size);
-			}
-		}
-		if (swapped == 0)
-			break;
-	}
 }

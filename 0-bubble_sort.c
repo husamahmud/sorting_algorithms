1dcf3bd932b93646da3459f57fef481627019f95
iamhusam <husamahmud@gmail.com>
2023-11-17 16:23:37 +0300

    Edit 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -32,9 +32,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				print_array(array, size);
 				swap(&array[j], &array[j + 1]);
 				swapped = 1;
+				print_array(array, size);
 			}
 		}
 		if (swapped == 0)

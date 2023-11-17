e3e35ca7c15e63af93b4a2077dfc59dcd89511a8
iamhusam <husamahmud@gmail.com>
2023-11-17 21:27:10 +0300

    Edit 1->2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,5 @@
 #include "sort.h"
 
-void swap_nodes(listint_t **list, listint_t *curr, listint_t *prev_node);
-
 /**
  * insertion_sort_list - Insertion Sort Algorithm.
  *
@@ -13,46 +11,19 @@ void insertion_sort_list(listint_t **list)
 	listint_t *prev_node;
 	int data;
 
-	/* check for invalid input or lists wiht 0 or 1 nodes */
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	/* iterate over the list starting from the second node */
 	for (curr = (*list)->next; curr != NULL; curr = curr->next)
 	{
 		data = curr->n;
 		prev_node = curr->prev;
 
-		/* move the curr node backward till it reaches the correct position */
 		while (prev_node && prev_node->n > data)
 		{
-			/* swap the curr node with its prev node */
 			swap_nodes(list, curr, prev_node);
 			print_list(*list);
 			prev_node = curr->prev;
 		}
 	}
-}
-
-/**
- * swap_nodes - Swaps two nodes in a doubly linked list.
- *
- * @list: Double pointer to the head of the list
- * @curr: Node to be swapped.
- * @prev_node: Previous node of the node to be swapped.
- */
-void swap_nodes(listint_t **list, listint_t *curr, listint_t *prev_node)
-{
-	prev_node->next = curr->next;
-	if (curr->next)
-		curr->next->prev = prev_node;
-
-	curr->next = prev_node;
-	curr->prev = prev_node->prev;
-	prev_node->prev = curr;
-
-	if (curr->prev)
-		curr->prev->next = curr;
-	else
-		*list = curr;
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,5 @@
 #include "sort.h"
 
-void swap(int *x, int *y);
-
 /**
  * selection_sort - Selection Sort Algorithm.
  *
@@ -13,38 +11,23 @@ void selection_sort(int *array, size_t size)
 	size_t i, j;
 	size_t idx;
 
-	/* check for invalid input or arrays with 1 or fewer elements */
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
 		idx = i;
-		/* find the index of the minimum element in the remaining portion */
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[idx])
 				idx = j;
 		}
 
-		/* swap the minimum element with the current element, if necessary */
 		if (idx != i)
 		{
 			swap(&array[idx], &array[i]);
 			print_array(array, size);
 		}
 	}
-}
-
-/**
- * swap - Swaps two values of two integers.
- *
- * @x: Pointer to the first integer.
- * @y: Pointer to the second integer.
- */
-void swap(int *x, int *y)
-{
-	int temp = *x;
-	*x = *y;
-	*y = temp;
 }

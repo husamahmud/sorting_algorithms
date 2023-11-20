a6588e07ac0dea924afe58def393729957318c3f
iamhusam <husamahmud@gmail.com>
2023-11-20 03:07:55 +0300

    Edit 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,5 +1,7 @@
 #include "sort.h"
 
+void swap(int *x, int *y);
+
 /**
  * shell_sort - Shell Sort Algorithm.
  *
@@ -9,18 +11,37 @@
 void shell_sort(int *array, size_t size)
 {
 	size_t i, j;
-	size_t interval;
+	size_t interval = 1;
 	int temp;
 
-	for (interval = size * (3 + 1); interval > 0; interval /= (size * (3 + 1)))
+	if (array == NULL || size < 2)
+		return;
+
+	while (interval < size / 3)
+		interval = 3 * interval + 1;
+
+	while (interval >= 1)
 	{
-		for (i = interval; i < size; i += 1)
+		for (i = interval; i < size; i++)
 		{
 			temp = array[i];
 			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
-				array[j] = array[j - interval];
-			array[j] = temp;
+				swap(&array[j], &array[j - interval]);
 		}
 		print_array(array, size);
+		interval /= 3;
 	}
+}
+
+/**
+ * swap - Swaps two values of two integers.
+ *
+ * @x: Pointer to the first integer.
+ * @y: Pointer to the second integer.
+ */
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
 }

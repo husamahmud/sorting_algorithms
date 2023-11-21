384503b79ae085ff7de40f0b89d2b3419dda8f63
iamhusam <husamahmud@gmail.com>
2023-11-22 02:31:02 +0300

    Edit 8

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -3,57 +3,65 @@
 void swap(int *x, int *y);
 
 /**
- * sift_down - Restores the heap property by sifting down the element at the
- * given index.
- *
- * @array: Pointer to the array.
- * @start: The start index of the sub-heap.
- * @end: The end index of the sub-heap.
+ * check_swiftdown - checks the heap property and swaps if necessary
+ * @array: Pointer to the array to be sorted.
  * @size: The size of the array.
+ * @size_init: original size of the array
+ * @root: index of the root of the tree
  */
-void sift_down(int *array, size_t start, size_t end, size_t size)
+void check_swiftdown(int *array, size_t size_init, size_t size, size_t root)
 {
-	size_t root = start;
+	size_t left_child, right_child;
+	int left_child_val, right_child_val;
 
-	while (root * 2 + 1 <= end)
+	left_child = root * 2 + 1;
+	right_child = left_child + 1;
+	left_child_val = array[left_child];
+	right_child_val = array[right_child];
+
+	if (((left_child < size) && (right_child < size) &&
+	     (left_child_val >= right_child_val && left_child_val > array[root])) ||
+	    ((left_child == size - 1) && left_child_val > array[root]))
 	{
-		size_t child = root * 2 + 1;
-		size_t swap_idx = root;
-
-		if (array[swap_idx] < array[child])
-			swap_idx = child;
-		if (child + 1 <= end && array[swap_idx] < array[child + 1])
-			swap_idx = child + 1;
-		if (swap_idx == root)
-			return;
-
-		swap(&array[root], &array[swap_idx]);
-		print_array(array, size);
-
-		root = swap_idx;
+		swap(&array[root], &array[left_child]);
+		print_array(array, size_init);
+	} else if ((left_child < size) && (right_child < size) &&
+	           (right_child_val > left_child_val &&
+	            right_child_val > array[root]))
+	{
+		swap(&array[root], &array[right_child]);
+		print_array(array, size_init);
 	}
+
+	if (left_child < size - 1)
+		check_swiftdown(array, size_init, size, left_child);
+	if (right_child < size - 1)
+		check_swiftdown(array, size_init, size, right_child);
 }
 
 /**
- * heap_sort - Heap Sort Algorithm.
+ * heap_sort - Heap sort algorithm.
  *
  * @array: Pointer to the array to be sorted.
  * @size: The size of the array.
  */
 void heap_sort(int *array, size_t size)
 {
-	size_t i;
+	size_t i, size_init = size;
 
-	if (array == NULL || size < 2)
+	if (!array)
 		return;
 
-	for (i = size / 2; i > 0; i--)
-		sift_down(array, i - 1, size - 1, size);
-
-	for (i = size - 1; i > 0; i--)
+	for (i = 0; i < size / 2; i++)
 	{
-		swap(&array[0], &array[i]);
-		sift_down(array, 0, i - 1, size);
+		check_swiftdown(array, size_init, size, size / 2 - 1 - i);
+	}
+
+	for (i = 0; i < size_init - 1; i++)
+	{
+		swap(&array[0], &array[size - 1 - i]);
+		print_array(array, size_init);
+		check_swiftdown(array, size_init, size - i - 1, 0);
 	}
 }
 
